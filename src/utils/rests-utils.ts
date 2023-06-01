259651a7c7069bb3f6039ec7a878b3825fe88d01
screampilots <screampilots@github.com>
2023-06-01 17:59:40 +0300

    Updates

--- src/utils/rests-utils.ts
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = 'https://norma.nomoreparties.space/api';
-
-export const checkResponse = (res: Response) => {
-    return res.ok ? res.json() : res.json().then((err) => Promise.reject(err))
-};
-
-export function request(url: string, options: any) {
-    return fetch(url, options).then(checkResponse)
-};
